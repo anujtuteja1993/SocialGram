ad95d3ff62815fb5e3b1758909dc550bbdaf25d1
Anuj Tuteja <anujtuteja1993@gmail.com>
2024-05-08 22:45:30 +1200

    Added TopBar for mobile design

--- src/Root/RootLayout.tsx
+++ src/Root/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Bottombar from "../components/Bottombar";
+import Topbar from "../components/Topbar";
 
 const RootLayout = () => {
     return (
         <div className="w-full md:flex">
+            <Topbar />
             <Sidebar />
             <section className="flex flex-1 h-full w-auto justify-center">
                 <Outlet />
---
+++ src/components/Topbar/index.tsx
@@ -0,0 +1,20 @@
+import logo from "../../assets/logo.png";
+import { ArrowLeftStartOnRectangleIcon as LogoutButton } from "@heroicons/react/24/outline";
+
+const Topbar = () => {
+    return (
+        <div className="flex justify-between w-full fixed top-0 bg-base-100 md:hidden">
+            <div className="flex flex-row justify-center items-center ml-1">
+                <img src={logo} className="w-12 h-12 inline-block" alt="logo" />
+                <h1 className="font-bold text-xl">SocialGram</h1>
+            </div>
+            <div className="flex items-center mr-1">
+                <button className="h-[30px] w-[30px] inline-flex justify-center items-center rounded-lg transition hover:scale-110 active:scale-90">
+                    <LogoutButton className="h-[30px] w-[30px]" />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Topbar;
