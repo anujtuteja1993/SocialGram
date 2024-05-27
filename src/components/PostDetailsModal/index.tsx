7f1d4864a4b9597e6234aa6f292cd529638ba7e0
Anuj Tuteja <anujtuteja1993@gmail.com>
2024-05-28 00:45:33 +1200

    Added PostDetails Modal

---
+++ src/components/PostDetailsModal/index.tsx
@@ -0,0 +1,33 @@
+import { Models } from "appwrite";
+import PostCard from "../PostCard";
+import { useUserContext } from "../../contexts/userContext";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
+import { MdClose } from "react-icons/md";
+
+type PostDetailsModalProps = {
+    post?: Models.Document;
+};
+
+const PostDetailsModal = ({ post }: PostDetailsModalProps) => {
+    const { user } = useUserContext();
+
+    return (
+        <dialog id="postModal" className="modal modal-middle">
+            <div className="flex modal-box w-full overflow-hidden justify-center">
+                <form method="dialog">
+                    {user.id === post?.creator.$id && (
+                        <button className="h-[20px] w-[20px] md:h-[25px] md:w-[25px] absolute left-3 top-3">
+                            <HiOutlinePencilSquare className="h-full w-full" />
+                        </button>
+                    )}
+                    <button className="h-[20px] w-[20px] md:h-[25px] md:w-[25px] btn-circle btn-ghost absolute right-3 top-3">
+                        <MdClose className="h-full w-full" />
+                    </button>
+                </form>
+                {post && <PostCard post={post} />}
+            </div>
+        </dialog>
+    );
+};
+
+export default PostDetailsModal;
